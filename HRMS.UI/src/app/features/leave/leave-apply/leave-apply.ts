9d61c934acf004aa20cc16487e86890161a27f56
Jyotiranjan Sahoo <jyotiranjansahoo556@gmail.com>
2026-06-20 13:08:09 +0530

    Add Angular HRMS UI project

---
+++ HRMS.UI/src/app/features/leave/leave-apply/leave-apply.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-apply',
+  imports: [],
+  templateUrl: './leave-apply.html',
+  styleUrl: './leave-apply.css',
+})
+export class LeaveApply {}
